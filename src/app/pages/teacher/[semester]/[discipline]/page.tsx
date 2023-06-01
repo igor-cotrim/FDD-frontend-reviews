6c67015299cc1be7e08ec00b61b7ab75c7a6681c
Iago Rocha Porto <iagorochaporto@gmail.com>
2023-06-01 10:36:11 -0300

    fix: remover posição absoluta de ícones dentro dos buttons

--- src/app/pages/teacher/[semester]/[discipline]/page.tsx
+++ src/app/pages/teacher/[semester]/[discipline]/page.tsx
@@ -22,7 +22,7 @@ const Discipline = () => {
           Programação para Web - 2022.2
         </h1>
         <Button type="button" className="max-w-xs">
-          <PlusIcon className="absolute w-6 h-6" />
+          <PlusIcon className="w-6 h-6" />
           Adicionar discente
         </Button>
       </div>
