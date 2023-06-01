0f5835ad47e9def1e1fbe980883c26a956ec79d6
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-06-01 10:45:37 -0300

    feat: created page and data for discente route

---
+++ src/app/pages/teacher/[semester]/[discipline]/[discente]/page.tsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { ArrowLeftIcon, PlusIcon } from "@heroicons/react/24/outline";
+
+import { Button, Table } from "@/components";
+
+import * as D from "./data";
+
+const Discente = () => {
+  const router = useRouter();
+
+  return (
+    <div className="pt-12">
+      <div className="flex items-center justify-between mb-4">
+        <h1
+          className="flex items-center font-mono text-3xl font-semibold cursor-pointer text-primary"
+          onClick={() => router.back()}
+        >
+          <ArrowLeftIcon className="w-6 h-6 mr-3" />
+          Programação para Web - 2022.2
+        </h1>
+        <Button type="button" className="max-w-xs">
+          <PlusIcon className="absolute w-6 h-6" />
+          Adicionar discente
+        </Button>
+      </div>
+      <Table
+        columns={D.Data().columns}
+        data={D.Data().data}
+      />
+    </div>
+  );
+};
+
+export default Discente;
--- src/app/pages/teacher/[semester]/[discipline]/data.tsx
+++ src/app/pages/teacher/[semester]/[discipline]/data.tsx
@@ -1,8 +1,10 @@
+import { removeSpacesAndAccents } from "@/utils";
 import {
   PencilSquareIcon,
   EyeIcon,
   TrashIcon,
 } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
 
 export const Data = () => ({
   data: [
@@ -52,11 +54,13 @@ export const Data = () => ({
 });
 
 export const Actions = () => {
+  const router = useRouter();
+  
   const viewAction = (row: any) => (
     <EyeIcon
       className="p-2 text-blue-600 rounded-lg cursor-pointer w-9 h-9"
       title="Visualizar"
-      onClick={() => console.log(row)}
+      onClick={() => router.push(`docente/semester/discipline/${removeSpacesAndAccents(row.original.name)}`)}
     />
   );
   const editAction = (row: any) => (
