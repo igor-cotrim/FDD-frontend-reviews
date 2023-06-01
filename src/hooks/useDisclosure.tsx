068142432964dee64c2f81d4b7d5f8c4b1a6e458
Iago Rocha Porto <iagorochaporto@gmail.com>
2023-06-01 13:44:36 -0300

    feat: adicionar hook useDisclosure

---
+++ src/hooks/useDisclosure.tsx
@@ -0,0 +1,17 @@
+import { useCallback, useState } from "react";
+
+export function useDisclosure() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onOpen = useCallback(() => setIsOpen(true), [])
+  const onClose = useCallback(() => setIsOpen(false), [])
+  const onToggle = useCallback(() => setIsOpen((isOpen) => !isOpen), [])
+
+
+  return {
+    isOpen,
+    onOpen,
+    onClose,
+    onToggle,
+  };
+}
