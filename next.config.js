0e3475ed15c0d25b285557091200c557ca44491f
Igor Cotrim <igorcotrim.dev@gmail.com>
2023-06-05 16:31:46 -0300

    feat: :sparkles: add secretary pages

--- next.config.js
+++ next.config.js
@@ -13,6 +13,26 @@ const nextConfig = {
       source: "/docente/:semestre/:disciplina",
       destination: "/pages/teacher/[semester]/[discipline]",
     },
+    {
+      source: "/secretaria",
+      destination: "/pages/secretary",
+    },
+    {
+      source: "/secretaria/docentes",
+      destination: "/pages/secretary/teachers",
+    },
+    {
+      source: "/secretaria/discentes",
+      destination: "/pages/secretary/students",
+    },
+    {
+      source: "/secretaria/disciplinas",
+      destination: "/pages/secretary/disciplines",
+    },
+    {
+      source: "/secretaria/semestres",
+      destination: "/pages/secretary/semesters",
+    },
   ],
 };
 
---
+++ src/app/pages/secretary/students/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { ArrowLeftIcon, PlusIcon } from "@heroicons/react/24/outline";
+
+import { Button, Table } from "@/components";
+
+import * as D from "./data";
+import { useModalStore } from "@/store";
+
+const Teacher = () => {
+  const router = useRouter();
+  const { toggleVisibility } = useModalStore();
+
+  return (
+    <div className="pt-12">
+      <div className="flex items-center justify-between mb-4">
+        <h1
+          className="flex items-center font-mono text-3xl font-semibold cursor-pointer text-primary"
+          onClick={() => router.back()}
+        >
+          <ArrowLeftIcon className="w-6 h-6 mr-3" />
+          Discentes
+        </h1>
+        <Button
+          type="button"
+          className="max-w-[12rem]"
+          onClick={() => {
+            toggleVisibility(true, "form");
+          }}
+        >
+          <PlusIcon className="w-6 h-6" />
+          Adicionar discente
+        </Button>
+      </div>
+      <Table
+        columns={D.Data().columns}
+        data={D.Data().data}
+        actions={D.Actions()}
+      />
+    </div>
+  );
+};
+
+export default Teacher;
